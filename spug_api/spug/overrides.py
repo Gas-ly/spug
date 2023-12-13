93e8065b53ba387e649d836e94d845e4e2213838
Gasly <36955785+Gas-ly@users.noreply.github.com>
2023-12-13 11:55:48 +0800

    Update overrides.py

--- spug_api/spug/overrides.py
+++ spug_api/spug/overrides.py
@@ -7,7 +7,7 @@ DATABASES = {
         'NAME': 'spug',             # 替换为自己的数据库名，请预先创建好编码为utf8mb4的数据库
         'USER': 'root',        # 数据库用户名
         'PASSWORD': 'Txe2SbMjt3',  # 数据库密码
-        'HOST': 'musql-svc',        # 数据库地址
+        'HOST': 'mysql-svc',        # 数据库地址
         'OPTIONS': {
             'charset': 'utf8mb4',
             'sql_mode': 'STRICT_TRANS_TABLES',
